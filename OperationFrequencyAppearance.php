09a3a6d0c1e99b9c07791ebdda1e8c398fea95fe
Shunsuke-Komuta <komuta@level-five.jp>
2017-05-10 00:08:02 +0900

    format by php-cs-fixer

--- OperationFrequencyAppearance.php
+++ OperationFrequencyAppearance.php
@@ -1,7 +1,7 @@
 <?php
 
-class OperationFrequencyAppearance {
-
+class OperationFrequencyAppearance
+{
     private $count;
 
     public function output($numbers)
@@ -15,7 +15,9 @@ class OperationFrequencyAppearance {
     private function calculate($numbers)
     {
         $array = array_count_values($numbers);
-        if ($this->isEnd($array)) return $numbers;
+        if ($this->isEnd($array)) {
+            return $numbers;
+        }
 
         $this->count++;
         $getFrequency = function ($value) use (&$array) {
@@ -26,7 +28,7 @@ class OperationFrequencyAppearance {
 
     private function isEnd($array)
     {
-        return empty(array_filter($array, function($key, $value) {
+        return empty(array_filter($array, function ($key, $value) {
             return $key != $value;
         }, ARRAY_FILTER_USE_BOTH));
     }
