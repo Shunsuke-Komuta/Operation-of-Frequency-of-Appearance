584295393da35c0358c978f971bb53dd92737ad7
Shunsuke-Komuta <komuta@level-five.jp>
2017-05-24 17:32:46 +0900

    remove OperationFrequencyAppearance.php

--- index.php
+++ index.php
@@ -1,13 +1,35 @@
 <?php
 
-require('OperationFrequencyAppearance.php');
+function output()
+{
+    $count = 0;
+    $calculate = function ($numbers) use (&$count, &$calculate) {
+        $counter = array_count_values($numbers);
+        $getFrequency = function ($value) use (&$counter) {
+            return $counter[$value];
+        };
+        $nextData = array_map($getFrequency, $numbers);
+        if ($numbers == $nextData) {
+            return array($count, $numbers);
+        }
+        $count++;
+        return $calculate($nextData);
+    };
+    return $calculate;
+}
 
-$operationFrequencyAppearance = new OperationFrequencyAppearance;
 while (true) {
     $num = trim(fgets(STDIN));
     if ($num == 0) {
         return;
     }
     $line = trim(fgets(STDIN));
-    $operationFrequencyAppearance->output(explode(" ", $line));
+    $data = explode(" ", $line);
+
+    $outputFunc = output();
+    $numAndResult = $outputFunc($data);
+    $num = $numAndResult[0];
+    $result = $numAndResult[1];
+    echo $num . "\n";
+    echo implode(" ", $result) . "\n";
 }
